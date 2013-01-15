fca260a84ccf40358e244776a44e589c2cc96428
anshulgarg <anshul@anshulgarg.com>
2013-01-16 05:16:45 +0800

    Added build file

---
+++ build.sh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/Users/agarg/.rvm/gems/ruby-1.9.3-p362@Blog/bin:/Users/agarg/.rvm/gems/ruby-1.9.3-p362@global/bin:/Users/agarg/.rvm/rubies/ruby-1.9.3-p362/bin:/Users/agarg/.rvm/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/git/bin
+
+rvm use 1.9.3-p362
+
+bundle install
